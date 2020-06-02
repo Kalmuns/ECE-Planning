9cfbdc3636b147776a578bff32dc78f73f7f41f9
Kruskal <vic.delerme@gmail.com>
2020-06-03 01:39:22 +0200

    Cell Calendrier Ajout du Brouillon de la Cell

--- src/Calendrier.java
+++ src/Calendrier.java
@@ -10,7 +10,8 @@ public class Calendrier extends JPanel {
 	
 	
 	public Calendrier() {
-		this.setLayout(new GridLayout(7,7));
+		//this.setLayout(new GridLayout(7,7));
+		this.setLayout(new GridLayout(7,7,10,10));
 		for(int i=0;i<7*7;i++)
 		{
 			this.add(new Cell());
--- src/Cell.java
+++ src/Cell.java
@@ -1,4 +1,6 @@
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridLayout;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
@@ -8,10 +10,30 @@ public class Cell extends JPanel{
 
 	
 	public Cell() {
-		this.setLayout(new BorderLayout());
-		add(new JButton("promo"),BorderLayout.NORTH);
-		add(new JButton("enseignant"),BorderLayout.CENTER);
-		add(new JButton("matiere"),BorderLayout.SOUTH);
+		this.setLayout(new GridLayout(0,1));
+		JButton Nom = new JButton("Nom et Type de Cours");
+		Nom.setBorderPainted(false);
+		Nom.setBackground(Color.GRAY);
+		add(Nom,BorderLayout.NORTH);
+		
+		JButton Prof = new JButton("Enseignant");
+		Prof.setBorderPainted(false);
+		Prof.setBackground(Color.GRAY);
+		add(Prof,BorderLayout.CENTER);
+		
+		JButton Td = new JButton("TD et Groupe");
+		Td.setBorderPainted(false);
+		Td.setBackground(Color.GRAY);
+		add(Td,BorderLayout.SOUTH);
+		
+		
+		JButton Salle = new JButton("Salle et Batiment");
+		Salle.setBorderPainted(false);
+		Salle.setBackground(Color.GRAY);
+		add(Salle,BorderLayout.SOUTH);
+		
+		
+		
 		this.validate();
 		// TODO Auto-generated constructor stub
 		// Case du tableaux n'est qu'un display les info passe par le constructeur. 
