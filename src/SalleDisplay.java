d878b3f0af6f889403e6ff9268c1008778e00c2d
Kruskal <vic.delerme@gmail.com>
2020-06-03 15:00:20 +0200

    Ajout des Display

---
+++ src/SalleDisplay.java
@@ -0,0 +1,28 @@
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class SalleDisplay extends JPanel {
+	private ArrayList<Salle> todisplay=null;
+	private ArrayList<JButton> boutton = new ArrayList<JButton>();
+	public SalleDisplay(ArrayList<Salle> gr)
+	{
+		int i=0;
+		todisplay=gr;
+		this.setLayout(new GridLayout(1,gr.size()));
+		for(i=0;i<gr.size();i++)
+		{
+			boutton.add(new JButton(new String("gr"+gr.get(i).getnom())));
+			
+			boutton.get(i).setBorderPainted(false);
+			boutton.get(i).setBackground(Color.GRAY);
+			
+			this.add(boutton.get(i));
+		}
+		this.setVisible(true);
+		this.validate();
+	}
+}
