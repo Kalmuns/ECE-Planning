a6e33fd0d3c4e2d9d3fecb2715b6eb86d81d3d8f
Kruskal <vic.delerme@gmail.com>
2020-06-07 21:07:05 +0200

    Merge

--- src/Calendrier.java
+++ src/Calendrier.java
@@ -11,7 +11,12 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-
+/**
+ * Ceci est un commentaire Javadoc. 
+ * Il commence par un slash suivis de deux étoiles. 
+ * Chaque ligne doit ensuite commencer par une étoile. 
+ * Enfin, il fini par une étoile suivie d'un slash.
+ */
 
 public class Calendrier extends JPanel {
 	
